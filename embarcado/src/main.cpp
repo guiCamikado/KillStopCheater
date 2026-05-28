f7a228b046e93fe3240d3056849b785b140b6b28
guiCamikado <guilherme.camikado@gmail.com>
2026-05-28 14:13:04 -0300

    Added abstraction level in APIs

--- embarcado/src/main.cpp
+++ embarcado/src/main.cpp
@@ -5,11 +5,9 @@
 WifiConnection wifi;
 
 void setup() {
-    Serial.begin(115200);
-    // wifi.startWifi("ESP32", "12345678");
-    wifi.connectWifi("Camikado2.4-5GHz", "a7a9y6A2@");
+  Serial.begin(115200);
+  // wifi.startWifi("ESP32", "12345678");
+  wifi.connectWifi("Camikado2.4-5GHz", "a7a9y6A2@");
 }
 
-void loop() {
-    wifi.handle();
-}
+void loop() { wifi.handle(); }
