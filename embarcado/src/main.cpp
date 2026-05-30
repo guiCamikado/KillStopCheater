fa227d88324d6575e2a6b62576685a52481df18b
guiCamikado <guilherme.camikado@gmail.com>
2026-05-30 18:00:19 -0300

    deletado bibliotecas não utilizadas e implementação de página http no ESP32

--- embarcado/src/main.cpp
+++ embarcado/src/main.cpp
@@ -10,21 +10,12 @@ Servo servo;
 
 void setup() {
   Serial.begin(115200);
-  // wifi.startWifi("ESP32", "12345678");
-  wifi.connectWifi("Camikado2.4-5GHz", "a7a9y6A2@");
+  wifi.startWifi("ESP32", "camikado");
+  // wifi.connectToWifi("Camikado2.4-5GHz", "a7a9y6A2@");
   wifi.api.servo.begin();
   wifi.api.initiateRoutes();
+  wifi.led.setup();
 }
-
-// void setup() {
-//   Serial.begin(115200);
-//   servo.setPeriodHertz(50);
-//   servo.attach(25, 500, 2400);
-//   delay(1000);
-//   servo.write(90);
-//   delay(2000);
-//   servo.write(0);
-// }
 
 void loop() {
   wifi.handle();
